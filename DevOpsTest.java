7c79b73a1aa81b12e678cc6174a596b2c559ad29
Tejaswini Muley <52561179+TheConvincer@users.noreply.github.com>
2019-07-08 16:46:10 +0530

    uploaded on GIT for Jenkins linking ver-1.0
    right now i am just uploading java link files for jenkins.........................in next update ill write about steps

---
+++ DevOpsTest.java
@@ -0,0 +1,15 @@
+public class DevOpsTest 
+{
+  public static void main(String[] args)
+  {
+	  
+     String myNa = "Teja";
+     int myAge = 25;
+  
+     System.out.println("name is :" + myNa + "  my age is :" + myAge);
+
+     return ;
+  }
+}
+
+// C:\Program Files (x86)\Common Files\Oracle\Java\javapath;
